750d49a618f3c631c4cfe6b9ec29529225a4d0d8
Preston Kwei <prestonkwei@gmail.com>
2024-03-04 01:20:42 -0500

    More updates

--- upload.js
+++ upload.js
@@ -3,45 +3,46 @@ let myToken = '';
 document.getElementById('uploadButton').addEventListener('click', function() {
   if (!myToken) {
     myToken = prompt("Please enter your password");
-  }
-});
-
-let newFileName =''
-document.getElementById('uploadButton').addEventListener('click', function() {
-  const fileInput = document.getElementById('fileInput');
-  if (fileInput.files.length > 0) {
-    const file = fileInput.files[0];
-    const uuid = crypto.randomUUID();
-    newFileName = `${uuid}.png`;
-
-    uploadToGitHub(file, newFileName);
-  }
-});
-
-function uploadToGitHub(file, fileName) {
-  const reader = new FileReader();
-  reader.onload = function() {
-    const content = reader.result.split(',')[1];
-    fetch('https://api.github.com/repos/preston-k/cdn/contents/' + fileName, {
-      method: 'PUT',
-      headers: {
-        'Authorization': myToken,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        message: `uploading ${fileName}`,
-        content: content
-      })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      alert('File uploaded successfully! You can access it at cdn.prestonkwei.com/'+newFileName);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      alert('An error occurred!');
+    let newFileName =''
+    document.getElementById('uploadButton').addEventListener('click', function() {
+      const fileInput = document.getElementById('fileInput');
+      if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        const uuid = crypto.randomUUID();
+        newFileName = `${uuid}.png`;
+    
+        uploadToGitHub(file, newFileName);
+      }
     });
-  };
-  reader.readAsDataURL(file);
-}
+    
+    function uploadToGitHub(file, fileName) {
+      const reader = new FileReader();
+      reader.onload = function() {
+        const content = reader.result.split(',')[1];
+        fetch('https://api.github.com/repos/preston-k/cdn/contents/' + fileName, {
+          method: 'PUT',
+          headers: {
+            'Authorization': myToken,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            message: `uploading ${fileName}`,
+            content: content
+          })
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+          alert('File uploaded successfully! You can access it at cdn.prestonkwei.com/'+newFileName);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          alert('An error occurred!');
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+    
+    
+  }
+})
