e4ce9853132b1d9ccadd24f77794422762819d6d
Preston <71202878+preston-k@users.noreply.github.com>
2024-07-24 19:51:09 -0700

    Update ipblockcheck.js

--- ipblockcheck.js
+++ ipblockcheck.js
@@ -1,60 +1,62 @@
-let blockstatus = localStorage.getItem('prestonkwei-ipblock')
-if (blockstatus == 'true') {
-  document.querySelector('body').innerHTML = ''
-  window.location.replace('https://prestonkwei.com/ipblock')
-} else {
-  localStorage.setItem('prestonkwei-ipblock', 'false')
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+;(async () => {
   const supabaseUrl = 'https://ettzkxqykheylpckmsue.supabase.co'
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0dHpreHF5a2hleWxwY2ttc3VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyMTcyMzQsImV4cCI6MjAzMzc5MzIzNH0.cXg_8de7LNzuESXvGdcSqPvBVIy7iVF3jCN0LLYpFPw'
 
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
-  async function getUserIp() {
-      try {
-          const response = await fetch('https://api.ipify.org?format=json')
-          const data = await response.json()
-          return data.ip
-      } catch (error) {
-          console.error('Error getting user IP:', error)
-          return null
-      }
+  let blockstatus = localStorage.getItem('prestonkwei-ipblock')
+  if (blockstatus == 'true') {
+    document.querySelector('body').innerHTML = ''
+    window.location.replace('https://prestonkwei.com/ipblock')
+  } else {
+    localStorage.setItem('prestonkwei-ipblock', 'false')
   }
 
-  async function isUserIpInDatabase(userIp) {
+  document.addEventListener('DOMContentLoaded', () => {
+    async function getUserIp() {
+      try {
+        const response = await fetch('https://api.ipify.org?format=json')
+        const data = await response.json()
+        return data.ip
+      } catch (error) {
+        console.error('Error getting user IP:', error)
+        return null
+      }
+    }
+
+    async function isUserIpInDatabase(userIp) {
       const { data, error } = await supabase
-          .from('IP-BLOCKS')
-          .select('ip')
-          .eq('ip', userIp)
+        .from('IP-BLOCKS')
+        .select('ip')
+        .eq('ip', userIp)
 
       if (error) {
-          console.error('Error checking user IP:', error.message)
-          return false
+        console.error('Error checking user IP:', error.message)
+        return false
       } else if (data.length > 0) {
-          console.log('In DB:', data)
-          return true
+        console.log('In DB:', data)
+        return true
       } else {
-          console.log('Not in DB')
-          return false
+        console.log('Not in DB')
+        return false
       }
-  }
+    }
 
-  getUserIp().then(userIp => {
+    getUserIp().then(userIp => {
       if (userIp) {
-          isUserIpInDatabase(userIp).then(found => {
-              if (found) {
-                  document.querySelector('body').innerHTML = ''
-                  localStorage.setItem('prestonkwei-ipblock', 'true')
-                  window.location.replace('https://prestonkwei.com/ipblock')
-              } else {
-                  console.log('Not in DB')
-                  localStorage.setItem('prestonkwei-ipblock', 'false')
-              }
-          })
+        isUserIpInDatabase(userIp).then(found => {
+          if (found) {
+            document.querySelector('body').innerHTML = ''
+            localStorage.setItem('prestonkwei-ipblock', 'true')
+            window.location.replace('https://prestonkwei.com/ipblock')
+          } else {
+            console.log('Not in DB')
+            localStorage.setItem('prestonkwei-ipblock', 'false')
+          }
+        })
       } else {
-          console.log('Could not retrieve user IP.')
+        console.log('Could not retrieve user IP.')
       }
+    })
   })
-})
+})()
